0a62060572bd46ad8dc612e85bddbdd2e16c38ce
LahiruRJ <sanlahiru720@gmail.com>
2022-03-10 07:24:02 +0530

    Add controller files

---
+++ src/main/java/lk/ijse/dep8/controller/HomeFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep8.controller;
+
+import javafx.event.ActionEvent;
+
+public class HomeFormController {
+    public void btnArray_OnAction(ActionEvent event) {
+    }
+
+    public void btnCollection_OnAction(ActionEvent event) {
+    }
+
+    public void btnDB_OnAction(ActionEvent event) {
+    }
+}
