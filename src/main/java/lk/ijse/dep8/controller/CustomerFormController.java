41a9de4ca3d3e8b6b787774a935168eaa0967bea
LahiruRJ <sanlahiru720@gmail.com>
2022-03-10 09:13:18 +0530

    Implement load All customers & initialize method

--- src/main/java/lk/ijse/dep8/controller/CustomerFormController.java
+++ src/main/java/lk/ijse/dep8/controller/CustomerFormController.java
@@ -1,10 +1,63 @@
 package lk.ijse.dep8.controller;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.dep8.util.CustomerTM;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 public class CustomerFormController {
-    public TableView tblCustomers;
+    public TableView<CustomerTM> tblCustomers;
+    private Connection connection;
+    public void initialize() throws ClassNotFoundException {
+        Properties prop = new Properties();
+
+        try {
+            prop.load(this.getClass().getResourceAsStream("/application.properties"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Platform.runLater(()->{
+                new Alert(Alert.AlertType.ERROR,"Failed to load configurations ").show();
+            });
+            return;
+        }
+        tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        Class.forName("com.mysql.cj.jdbc.");
+        try {
+            String url = String.format("jdbc:mysql://%s:%s:/%s", prop.getProperty("app.ip"), prop.getProperty("app.port"), prop.getProperty("app.database"));
+            this.connection= DriverManager.getConnection(url,prop.getProperty("app.username"),prop.getProperty("app.password"));
+loadCustomers();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            Platform.runLater(()->{
+                new Alert(Alert.AlertType.ERROR,"failed to established the database conection").show();
+            });
+            return;
+        }
+
+    }
+
+    private void loadCustomers() throws SQLException {
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM  customer");
+
+        while(rst.next()){
+            tblCustomers.getItems().add(new CustomerTM(rst.getString("id")
+            rst.getString("name")
+            rst.getString("address")));
+        }
+    }
 
     public void btnShowCustomerReport_OnAction(ActionEvent event) {
     }
---
+++ src/main/java/lk/ijse/dep8/util/CustomerTM.java
@@ -0,0 +1,49 @@
+package lk.ijse.dep8.util;
+
+public class CustomerTM {
+    private String id;
+    private String name;
+    private String address;
+
+    public CustomerTM() {
+    }
+
+    public CustomerTM(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerTM{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
